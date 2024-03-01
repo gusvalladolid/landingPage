4390bdc9a6f36144727721217936cb4a1bd6546a
gusvalladolid <gusvalladolidtt@gmail.com>
2024-03-01 16:04:58 -0600

    final commit

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,27 +1,31 @@
+import { useLocation } from 'react-router-dom'
 import video from '../assets/video.mp4'
-import Header from '../components/header';
-import Content from '../components/content';
-import Footer from '../components/footer';
-
+import Header from '../components/header'
+import Content from '../components/content'
+import Footer from '../components/footer'
 
 const Home = () => {
-    return (
-    <div className='relative h-screen'>
+  const location = useLocation();
+
+  return (
+    <div className="relative h-screen">
       {/* Background Video Container */}
-      <div className='fixed top-0 left-0 w-full h-full overflow-hidden z-[-1]'>
-        <video autoPlay loop muted className='w-screen h-screen object-fill'>
-          <source src={video} type='video/mp4' />
+      <div className="fixed top-0 left-0 w-full h-full overflow-hidden z-[-1]">
+        {
+        <video autoPlay loop muted className="w-screen h-screen object-fill">
+          <source src={video} type="video/mp4" />
           Your browser does not support the video tag.
-        </video>
+        </video>  
+        }
       </div>
       {/* Content */}
-      <div className='relative z-10 h-full flex items-center justify-center'>
-      <Header/>
+      <div className="relative z-10 h-full flex items-center justify-center">
+        <Header loggedUser={location.state}/>
       </div>
-      <Content/>
-      <Footer/>
+      <Content />
+      <Footer />
     </div>
-    );
-};
+  )
+}
 
-export default Home;
+export default Home
