6e0efb7b14db75f983753ebd78f20aeba5712c51
gusvalladolid <gusvalladolidtt@gmail.com>
2024-03-01 20:18:36 -0600

    Api link changed

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,8 +3,7 @@ const Footer = () => {
 <footer className="bg-white rounded-lg shadow dark:bg-[#1d1d1d] m-4">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+            <a href="" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Fellisse</span>
             </a>
         </div>
